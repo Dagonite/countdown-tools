4025c92d0294a70e08801f88094750d3269d142a
Dagonite <samuel.stock@stfc.ac.uk>
2025-12-21 17:08:13 +0000

    Update letter distributions and bonus sets

--- scripts/distributions.py
+++ scripts/distributions.py
@@ -1,14 +1,15 @@
-"""Defines the letter counts which make up the consonant and vowel decks for the
-10 letters rounds in a game of Countdown. There are bonus letter sets which can
-be added to each deck to provide more varied games and increase the scope of
-possible words that can appear.
+"""Defines the letter counts which make up the consonant and vowel decks used on
+the game show Countdown. Because of certain letter counts and the limited
+vowel/consonant distributions allowed, there's roughly 3,300 words which
+essentially have a 0% chance of showing up. Although this is a small fraction of
+the overall word list.
 
-There is also a variable for determining the frequency of each of the three
-selection types:
+The rough popularity of the three vowel/consonant distributions used on the
+show are:
 
-3-vowels, 6-consonants (28% of rounds)
-4-vowels, 5-consonants (59% of rounds)
-5-vowels, 4-consonants (13% of rounds)
+3-vowels, 6-consonants (picked in 28% of rounds)
+4-vowels, 5-consonants (picked in 60% of rounds)
+5-vowels, 4-consonants (picked in 12% of rounds)
 """
 
 VOWEL_COUNTS = {
@@ -19,9 +20,11 @@ VOWEL_COUNTS = {
     "U": 7,
 }
 
+BONUS_VOWEL_SETS = [{}]
+
 CONSONANT_COUNTS = {
     "B": 2,
-    "C": 3,
+    "C": 2,
     "D": 6,
     "F": 2,
     "G": 4,
@@ -43,59 +46,28 @@ CONSONANT_COUNTS = {
     "Z": 1,
 }
 
-BONUS_VOWEL_SETS = [
-    {
-        "I": 1,
-        "O": 1,
-        "U": 1,
-    },
-    {
-        "A": 1,
-        "E": 1,
-        "O": 1,
-    },
-    {
-        "I": 1,
-        "U": 2,
-    },
-    {
-        "A": 2,
-        "E": 1,
-    },
-]
-
 BONUS_CONSONANT_SETS = [
     {
-        "H": 1,
-        "S": 1,
-        "T": 1,
+        "B": 1,
+        "L": 1,
         "V": 1,
+        "W": 1,
     },
     {
-        "R": 1,
-        "N": 1,
+        "C": 1,
+        "G": 1,
         "V": 1,
         "W": 1,
     },
     {
         "C": 2,
-        "K": 1,
+        "F": 1,
         "L": 1,
-    },
-    {
-        "F": 2,
-        "W": 1,
-        "Y": 1,
-    },
-    {
-        "B": 2,
-        "D": 1,
-        "M": 1,
     },
 ]
 
 VOWEL_FREQUENCIES = {
     "3-vowels": 0.28,
-    "4-vowels": 0.59,
-    "5-vowels": 0.13,
+    "4-vowels": 0.60,
+    "5-vowels": 0.12,
 }
